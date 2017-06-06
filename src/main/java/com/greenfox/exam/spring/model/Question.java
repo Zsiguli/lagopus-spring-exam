d2243e80e477e968b70f875e05b96048267f0070
zsiguli <markovichzsigmond@gmail.com>
2017-06-06 10:10:34 +0200

    chore(database): fill database with questions

--- src/main/java/com/greenfox/exam/spring/model/Question.java
+++ src/main/java/com/greenfox/exam/spring/model/Question.java
@@ -1,14 +1,20 @@
 package com.greenfox.exam.spring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "questions")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Question {
 
   @Id
   private Long id;
   private String question;
+  private String answer;
 }
