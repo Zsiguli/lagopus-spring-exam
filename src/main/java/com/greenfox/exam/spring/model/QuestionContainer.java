ae03a40846ecf37a0d2abc0fc0f92180c2b6c220
zsiguli <markovichzsigmond@gmail.com>
2017-06-06 09:38:39 +0200

    chore(QuestionContainer): model ready

---
+++ src/main/java/com/greenfox/exam/spring/model/QuestionContainer.java
@@ -0,0 +1,10 @@
+package com.greenfox.exam.spring.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionContainer {
+
+  private int id;
+  private List<Question> questions;
+}
