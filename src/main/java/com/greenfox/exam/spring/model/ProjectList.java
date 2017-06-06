527dae32c7bfb0dcdd57421af28d32de8e0d7a4f
zsiguli <markovichzsigmond@gmail.com>
2017-06-06 11:19:48 +0200

    chore(projectlist): model sturcture ready

--- src/main/java/com/greenfox/exam/spring/model/ProjectList.java
+++ src/main/java/com/greenfox/exam/spring/model/ProjectList.java
@@ -1,4 +1,11 @@
 package com.greenfox.exam.spring.model;
 
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class ProjectList {
+
+  private List<Project> projectList;
 }
