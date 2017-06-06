33cd643301bfb1c316d1c7ba227c544049cffb43
zsiguli <markovichzsigmond@gmail.com>
2017-06-06 11:08:06 +0200

    chore(answer): container structure ready

--- src/main/java/com/greenfox/exam/spring/model/AnswerContainer.java
+++ src/main/java/com/greenfox/exam/spring/model/AnswerContainer.java
@@ -1,4 +1,13 @@
 package com.greenfox.exam.spring.model;
 
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class AnswerContainer {
+
+  private int id;
+  private List<Answer> answers;
+
 }
